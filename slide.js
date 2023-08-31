2815979373bfd47d750182af844852120c78d678
Javillcam6 <javillcam.6@gmail.com>
2023-08-31 14:18:18 -0700

    Slide.js

--- slide.js
+++ slide.js
@@ -1,21 +1,8 @@
-const burger = document.querySelector('.burger')
-const enlaces = document.querySelector('.enlaces-menu')
-const barras = document.querySelectorAll('.burger span')
-
-
-burger.addEventListener('click', ()=> {
-    enlaces.classList.toggle('activado')
-    barras.forEach(child => {child.classList.toggle('animado')});
-})
-
-
-
-// Slider//////////////
-let slider = document.querySelector(".slider-containerr")
+let slider = document.querySelector(".slider-contenedor")
 let sliderIndividual = document.querySelectorAll(".contenido-slider")
 let contador = 1;
 let width = sliderIndividual[0].clientWidth;
-let intervalo = 3000;
+let intervalo = 4000;
 
 window.addEventListener("resize", function(){
     width = sliderIndividual[0].clientWidth;
@@ -29,7 +16,7 @@ setInterval(function(){
 
 function slides(){
     slider.style.transform = "translate("+(-width*contador)+"px)";
-    slider.style.transition = "transform .8s";
+    slider.style.transition = "transform 1.5s";
     contador++;
 
     if(contador == sliderIndividual.length){
